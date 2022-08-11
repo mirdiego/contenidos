a88dd114de1c325042b53d48b11b46a45c8ee6a1
Hernán Valdivieso <hfvaldivieso@uc.cl>
2022-08-11 13:27:00 -0400

    :star: Contenidos Semana 0

---
+++ semana-00/ejemplo4/main.py
@@ -0,0 +1,8 @@
+import os
+
+if __name__ == '__main__':
+    print("Me estoy ejecutando desde:")
+    path = os.getcwd()
+    print(path)
+    content = open("hello world.txt").read()
+    print(content)
